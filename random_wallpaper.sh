9f511d89d358d3ed22e5e4a3ecfedbf088f07979
Lorenzo Santini <47870176+santinilorenzo@users.noreply.github.com>
2022-03-16 10:40:16 +0100

    Add files via upload
    script

---
+++ random_wallpaper.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# check arguments
+if [ -z "$1" ]
+then
+	echo "Manca argomento directory wallpapers"
+	exit 1
+fi
+
+# where wallpapers are
+echo "=> Directory: $1"
+cd $1
+
+# export DBUS_SESSION_BUS_ADDRESS environment variable
+PID=$(pgrep -t tty2 gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
+# random wallpaper
+files=(*)
+echo "= Found wallpapers ="
+printf '%s\n' "${files[@]}"
+echo
+WALLPAPERS_NO=${#files[@]}
+echo "=> number of wallpapers: $WALLPAPERS_NO"
+RANDOM_WALLPAPER_INDEX=$(($RANDOM % WALLPAPERS_NO))
+echo "=> randomly selected wallpaper index: $RANDOM_WALLPAPER_INDEX"
+SELECTED_WALLPAPER=${files[RANDOM_WALLPAPER_INDEX]}
+echo "=> setting wallpaper [$SELECTED_WALLPAPER]"
+
+# apply
+gsettings set org.gnome.desktop.background picture-uri file:////home/lorenzo/Immagini/Sfondi/$SELECTED_WALLPAPER
